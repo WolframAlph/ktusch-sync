394ca8f9471a00cdc550e61bbc9d54a6aa63a20d
Yan Yanchii <yyanchiy@gmail.com>
2020-08-16 11:35:01 +0200

    replaced bash with sh

--- oauth_decrypt.sh
+++ oauth_decrypt.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_CREDENTIALS_DECRYPT" \
 --output business/oauth/credentials.dat business/oauth/credentials.dat.gpg
