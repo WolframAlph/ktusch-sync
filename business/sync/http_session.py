4042474f36a00243a5e724ecc815fc584ea62cce
Yan Yanchii <yyanchiy@gmail.com>
2020-08-19 09:17:15 +0200

    added retry on status code for every request

--- business/sync/http_session.py
+++ business/sync/http_session.py
@@ -1,16 +1,50 @@
 import os
+from functools import wraps
+import logging
+from time import sleep
 
 from requests import Session, Response, exceptions as http_exceptions
 from google.auth.transport.requests import Request
 
 from business.sync.auth import OAuth
 from business.apis import Google
+from business.rest.statuses import HTTP
 
 
 def status_hook(response: Response, *args, **kwargs):
     response.raise_for_status()
-
     return response
+
+
+def retry_on_status(retries, statuses):
+
+    def decorated(method):
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            nonlocal retries
+
+            try:
+                return method(self, *args, **kwargs)
+            except http_exceptions.HTTPError as e:
+                if e.response.status_code in statuses and retries:
+                    print(f"Failed to perform request with error message: {str(e)}")
+                    retry_after = e.response.headers.get('Retry-After')
+
+                    if not retry_after:
+                        print('No Rery-After header was provided, sleeping for 1 minute')
+                        sleep(60)
+                    else:
+                        logging.info(f"Required to sleep for {retry_after} seconds, sleeping...")
+                        sleep(retry_after)
+
+                    retries -= 1
+                    return wrapper(self, *args, **kwargs)
+                else:
+                    print(f"Failed to obtain response with error message: {str(e)}")
+                    raise
+        return wrapper
+
+    return decorated
 
 
 class AuthHttpSession(Session):
@@ -31,6 +65,7 @@ class AuthHttpSession(Session):
     def update_token(self):
         self.headers.update({'Authorization': "Bearer " + self.credentials.token})
 
+    @retry_on_status(retries=3, statuses=(HTTP.SERVER_ERROR, HTTP.CONNECTION_RESET_BY_PEER, HTTP.TOO_MANY_REQUESTS))
     def request(self, *args, **kwargs):
         if self.credentials is not None and self.credentials.expired:
             self.credentials.refresh(Request())
